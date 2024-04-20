b3aa214a7e5e8049ad49872e44f07505a3d48566
Alexandr <alexandrkruz76@gmail.com>
2024-04-21 00:24:44 +0300

    add:some-adaptive

--- js/main.js
+++ js/main.js
@@ -6,13 +6,38 @@ owl.owlCarousel(
         margin: 30,
         startPosition: 1,
         items: 3,
+        responsive : { 
+            1000 : {
+                margin: 20,
+            },
+
+                1200 : {
+                    margin: 30,
+            }
+        }
     }
 );
 
+
+
 $('.slider__btn--prew').click(function() {
     owl.trigger('prev.owl.carousel');
-})
+});
 
 $('.slider__btn--next').click(function() {
     owl.trigger('next.owl.carousel');
-})
+});
+
+// Nav icon
+const navBtn = document.querySelector('.nav__toggle');
+const menuIcon = document.querySelector('.menu-icon');
+const nav = document.querySelector('.nav')
+
+navBtn.onclick = function () {
+    nav.classList.toggle('nav--mobile')
+    menuIcon.classList.toggle('menu-icon-active');
+    document.body.classList.toggle('no-scroll');
+};
+
+
+
